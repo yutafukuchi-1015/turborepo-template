0639df64f9a93db8950ee69726c8b89248a31df4
yuuyuu <fufufu10155@gmail.com>
2024-12-21 04:43:50 +0900

    init

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -6,7 +6,7 @@ app.get("/", (c) => {
   return c.json({ message: "Hello Bun!" });
 });
 
-const port = 3000;
+const port = 8080;
 console.log(`Running at http://localhost:${port}`);
 
 export default {
