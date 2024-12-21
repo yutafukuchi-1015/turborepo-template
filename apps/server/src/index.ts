a6420ae7f1f4c4cdd0ddb4d3410b4a8e958f4515
yuuyuu <fufufu10155@gmail.com>
2024-12-21 16:08:32 +0900

    init rpc of hono

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,13 +1,23 @@
 import { Hono } from "hono";
+import { hc } from "hono/client";
+import { AnyZodObject, z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.json({ message: "Hello Bun!" });
+const schema = z.object({
+  id: z.number(),
+  name: z.string(),
 });
 
+const routes = app.get("/v1/users", async (c) => {
+  return c.json({ message: `test` });
+});
+
+export type AppType = typeof routes;
+export const client = hc<AppType>("http://localhost:8080/");
+
 const port = 8080;
-console.log(`Running at http://localhost:${port}`);
 
 export default {
   port,
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,14 @@
 import { Button } from "@repo/ui";
-export default function Home(): JSX.Element {
+import { client } from "../../server/src/index";
+import React from "react";
+
+export default async function Home() {
+  const res = client.v1.users.$get();
+  const data = await (await res).json();
+  console.log("ttttt");
   return (
     <>
-      <Button>hello</Button>
+      <Button>/vi/users post res is {data.message}</Button>
       <h1 className="text-3xl font-bold underline text-blue-700">
         Hello world!
       </h1>
