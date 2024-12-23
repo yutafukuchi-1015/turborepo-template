eac25598c2549ff4084133738dbe51d24b7cc6c9
yuuyuu <fufufu10155@gmail.com>
2024-12-24 04:37:35 +0900

    await params

--- apps/web/app/employees/[id]/page.tsx
+++ apps/web/app/employees/[id]/page.tsx
@@ -4,15 +4,21 @@ import { client } from "@/server/src/index";
 export default async function EmployeesPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
-  const res = client.employees[":id"].$get({ param: { id: params.id } });
-  const employee = await (await res).json();
+  const { id } = await params;
+  const res = await client.employees[":id"].$get({
+    param: { id: id },
+  });
+  const employee = await res.json();
 
   return (
     <div>
       <h1>Employee Detail</h1>
-      <p>{employee.id}</p>
+      <div className="flex">
+        <p>id</p>
+        <p>{employee.id}</p>
+      </div>
       <p>{employee.name}</p>
     </div>
   );
