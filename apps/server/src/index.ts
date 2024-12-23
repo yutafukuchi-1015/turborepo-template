623b738b85772da34802be89930e0fa0259a89fc
yuuyuu <fufufu10155@gmail.com>
2024-12-24 04:16:12 +0900

    comment

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -5,7 +5,7 @@ import { employees } from "./router/employees";
 const app = new Hono().route("/employees", employees);
 
 export type AppType = typeof app;
-export const client = hc<AppType>("http://localhost:8080/");
+export const client = hc<AppType>("http://localhost:8080/"); // FIXME use .env
 
 const port = 8080;
 
