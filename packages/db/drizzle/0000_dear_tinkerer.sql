9d95c74112fa2746955e5f408eca2f028ece83b2
yuuyuu <fufufu10155@gmail.com>
2024-12-27 23:25:52 +0900

    db: bun generate

---
+++ packages/db/drizzle/0000_dear_tinkerer.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "employees" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "employees_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"name" varchar(255) NOT NULL,
+	"department" integer
+);
