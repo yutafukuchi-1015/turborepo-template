570de7103bc88b38628454aa58721a463d552472
yuuyuu <fufufu10155@gmail.com>
2024-12-27 22:46:46 +0900

    db: schema

--- packages/db/src/schema/employees/validation.ts
+++ packages/db/src/schema/employees/validation.ts
@@ -2,8 +2,14 @@ import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { employees } from "./employees";
 import { z } from "zod";
 
-export const insertEmployeesSchema = createInsertSchema(employees);
-export const selectEmployeesSchema = createSelectSchema(employees);
+const extendSchema = z.object({
+  name: z.string().min(1).max(255),
+});
+
+export const insertEmployeesSchema =
+  createInsertSchema(employees).merge(extendSchema);
+export const selectEmployeesSchema =
+  createSelectSchema(employees).merge(extendSchema);
 
 export type InsertEmployees = z.infer<typeof insertEmployeesSchema>;
 export type Employees = z.infer<typeof selectEmployeesSchema>;
