dd21b6ec922b542675ca56fbd0303bb27207dd12
yuuyuu <fufufu10155@gmail.com>
2024-12-27 17:37:18 +0900

    "/employees" api error handling

--- apps/server/src/router/employees.ts
+++ apps/server/src/router/employees.ts
@@ -1,41 +1,58 @@
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 import { eq } from "drizzle-orm";
-import { z } from "zod";
+import { z, ZodObject, ZodTypeAny } from "zod";
 import { zValidator } from "@hono/zod-validator";
 import { db } from "@repo/db";
 import { employees as employeesTable } from "@repo/db/src/schema";
+import { selectEmployeesSchema } from "@repo/db/src/schema/employees/validation";
 
 export const employees = new Hono()
   .get("/", async (c) => {
-    const results = await db.select().from(employeesTable);
-    return c.json({ results });
+    const _employees = await db.select().from(employeesTable);
+
+    // if it occured error, catch error by app.onError as ZodError
+    const data = z.array(selectEmployeesSchema as any).parse(_employees);
+
+    return c.json({ results: data }, 200);
   })
   .get("/:id", async (c) => {
     const { id } = c.req.param();
-    const result = await db
+    const _employees = await db
       .select()
       .from(employeesTable)
       .where(eq(employeesTable.id, Number(id)));
+    const _employee = _employees[0];
 
-    if (!result[0]) {
-      throw new Error("Employee not found");
+    if (!_employee) {
+      throw new HTTPException(403, { message: "Forbidden" });
     }
 
-    return c.json({ ...result[0] });
+    // if it occured error, catch error by app.onError as ZodError
+    const data = selectEmployeesSchema.parse(_employee);
+
+    return c.json({ ...data }, 200);
   })
   .post(
     "/",
     zValidator(
       "json",
-      // insertEmployeesSchema),
+      // FIXME, insertEmployeesSchema,
       z.object({
         name: z.string(),
       })
     ),
     async (c) => {
       const body = await c.req.valid("json");
-      const res = await db.insert(employeesTable).values(body);
-      return c.json(res);
+      const _newEmployees = await db
+        .insert(employeesTable)
+        .values(body)
+        .returning();
+
+      // if it occured error, catch error by app.onError as ZodError
+      const data = selectEmployeesSchema.parse(_newEmployees);
+
+      return c.json(data, 200);
     }
   )
   .put(
@@ -50,15 +67,19 @@ export const employees = new Hono()
     async (c) => {
       const { id } = c.req.param();
       const body = await c.req.valid("json");
-      const res = await db
+      const _newEmployees = await db
         .update(employeesTable)
         .set(body)
-        .where(eq(employeesTable.id, Number(id)));
-      return c.json(res);
+        .where(eq(employeesTable.id, Number(id)))
+        .returning();
+
+      // if it occured error, catch error by app.onError as ZodError
+      const data = selectEmployeesSchema.parse(_newEmployees);
+      return c.json(data, 200);
     }
   )
   .delete("/:id", async (c) => {
     const { id } = c.req.param();
     await db.delete(employeesTable).where(eq(employeesTable.id, Number(id)));
-    return c.json({ success: true });
+    return c.json({ success: true }, 200);
   });
