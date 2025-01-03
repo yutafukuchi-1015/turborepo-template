812e1541c3228901793ed0beacab0573db9ec97e
yuuyuu <fufufu10155@gmail.com>
2025-01-03 23:02:09 +0900

    db: test

--- apps/server/src/router/employees/employees.test.ts
+++ apps/server/src/router/employees/employees.test.ts
@@ -1,26 +1,59 @@
-import { expect, test } from "bun:test";
+// Import necessary modules
+import { expect, test, afterAll } from "bun:test";
+
 import { testClient } from "hono/testing";
-
 import { app } from "../../index";
+import { db } from "@repo/db";
+import {
+  departments,
+  Employees,
+  employees,
+  InsertEmployees,
+} from "@repo/db/src/schema";
 
-test("/employees GET", async () => {
-  const res = await testClient(app).employees.$get();
-  const json = await res.json();
+// Seed data
+const seedData: InsertEmployees[] = [
+  { name: "Employee 1", department: 1 },
+  { name: "Employee 2", department: 2 },
+  // Add more seed data as needed
+];
 
-  console.log("json", json);
+const makeInitDb = async (
+  tx: Parameters<Parameters<typeof db.transaction>[0]>[0]
+) => {
+  await tx.delete(employees).execute();
+  await tx.insert(employees).values(seedData).execute();
+};
 
-  expect(res.status).toBe(200);
+// GET /employees test
+test("GET /employees", async () => {
+  await db.transaction(async (tx) => {
+    makeInitDb(tx);
+
+    const res = await testClient(app).employees.$get();
+    const json = await res.json();
+
+    expect(res.status).toBe(200);
+  });
 });
 
-// FIXME, After run test, undo db.
-test("/employees POST", async () => {
-  const res = await testClient(app).employees.$post({
-    json: {
-      name: "employees-post-test",
-      department: 1,
-    },
-  });
-  const json = await res.json();
+// POST /employees test
+test("POST /employees", async () => {
+  await db.transaction(async (tx) => {
+    makeInitDb(tx);
 
-  expect(res.status).toBe(200);
+    const mockData = {
+      json: {
+        name: "employees-post-test8",
+        department: 1,
+      },
+    };
+
+    const res = await testClient(app).employees.$post(mockData);
+    const json = await res.json();
+
+    expect(res.status).toBe(200);
+    expect(json.name).toBe(mockData.json.name);
+    expect(json.department).toBe(1);
+  });
 });
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,5 +2,5 @@ import "dotenv";
 import { drizzle } from "drizzle-orm/node-postgres";
 
 export const db = drizzle(
-  "postgres://postgres:postgres@localhost:5432/postgres", // FIXME use .env
+  "postgres://postgres:postgres@localhost:5432/postgres" // FIXME use .env
 );
